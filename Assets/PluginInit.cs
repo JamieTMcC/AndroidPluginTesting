1845a05bc7d6b7d13fafb4e477239de8ec6c1814
Jamie Mccready <2459354m@student.gla.ac.uk>
2023-06-27 07:49:50 +0100

    updated scripts

--- Assets/PluginInit.cs
+++ Assets/PluginInit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -41,11 +42,42 @@ public class PluginInit : MonoBehaviour
         Debug.Log("Time: " + result);
         return result;
     }
+    public string getRequest(){
+        string result = _pluginInstance.Call<string>("getHTML");
+       // Debug.Log("Request: " + result);
+        return result;
+    }
 
     public void toast(){
         if (_pluginInstance != null)
         {
-            _pluginInstance.Call("Toast", getTime().ToString());
+            var watch = new System.Diagnostics.Stopwatch();
+            //parse string as integer
+            int x = Int32.Parse(getRequest());
+            int y = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                
+                watch.Start();
+                for (int j = 0; j < 100; j++)
+                {
+                    y = Int32.Parse(getRequest());
+                    if (x != y - 1)
+                    {
+                        Debug.Log("Error: " + x + " " + y);
+                    }
+                    x++;
+                }
+                watch.Stop();
+                Debug.Log($"Execution Time: {watch.ElapsedMilliseconds} ms");
+                Debug.Log($"Execution Time per request: {watch.ElapsedMilliseconds / 100} ms");
+                watch.Restart();
+            }
+
+
+        }
+        else{
+            Debug.Log("Plugin error");
         }
     }
 
